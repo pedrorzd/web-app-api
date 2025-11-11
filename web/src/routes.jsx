329dad10bafcdcc4037e3215565bafaf90fd23dd
Pedro Rodrigues <pedrohrodrigues86@gmail.com>
2025-11-10 23:02:25 -0300

    organização projeto

--- web/src/routes.jsx
+++ web/src/routes.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Home from "./components/Home.jsx"; // Verifique se o nome está Maiúsculo
 import IdPage from "./components/IdPage.jsx"; // Verifique se o nome está Maiúsculo
-import Cadastro from "./components/Cadastro.js";
+import Cadastro from "./components/Cadastro.jsx";
 
 export const AppRoutes = () => {
     return (
